ce47b1103c5f16b4f7658f9cc7993a1156c17338
yzw <741646546@qq.com>
2019-09-03 12:59:22 +0800

    一把锁

---
+++ hrm_course_parent/hrm_course_service/src/main/java/com/yzw/config/MybatisPlusConfig.java
@@ -0,0 +1,18 @@
+package com.yzw.config;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+//Spring boot方式:配置分页插件
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.yzw.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+}
--- hrm_mybatisplus_gen/src/main/java/com/yzw/GenteratorCode.java
+++ hrm_mybatisplus_gen/src/main/java/com/yzw/GenteratorCode.java
@@ -17,7 +17,7 @@ public class GenteratorCode {
 
     public static void main(String[] args) throws InterruptedException {
         //用来获取Mybatis-Plus.properties文件的配置信息
-        ResourceBundle rb = ResourceBundle.getBundle("mybatisplus-course");
+        ResourceBundle rb = ResourceBundle.getBundle("mybatisplus-manage");
         AutoGenerator mpg = new AutoGenerator();
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
@@ -42,7 +42,9 @@ public class GenteratorCode {
         StrategyConfig strategy = new StrategyConfig();
         strategy.setTablePrefix(new String[] { "t_" });// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-        strategy.setInclude(new String[]{"t_course_type"}); // 需要生成的表
+        strategy.setInclude(new String[]{"t_department","t_employee","t_meal","t_menu","t_permission",
+                "t_role","t_tenant","t_tenant_type"
+        }); // 需要生成的表
         mpg.setStrategy(strategy);
         // 包配置
         PackageConfig pc = new PackageConfig();
